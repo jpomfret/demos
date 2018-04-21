64c609d4f06daebc599cadc3c45946e51b4749e9
Jess Pomfret <jpomfret7@gmail.com>
2018-04-21 16:24:28 -0700

    remove and restore db

--- DataCompression/01_Reset-Environment.ps1
+++ DataCompression/01_Reset-Environment.ps1
@@ -1,9 +1,13 @@
 Import-Module dbatools
 
-$config = Import-PowerShellDataFile -Path Config.psd1
+$path = Split-Path $MyInvocation.MyCommand.Path
+
+$config = Import-PowerShellDataFile -Path "$path\Config.psd1"
 
 Write-Output ("Running with Instance: {0}" -f $config.Instance2016 )
 Write-Output ("Running with Database: {0}" -f $config.Database )
-Write-Output ("Running with BackupFile: {0}" -f $config.Database )
+Write-Output ("Running with BackupFile: {0}" -f $config.BackupFile )
 
-#Remove-DbaDatabase -SqlInstance 
+Remove-DbaDatabase -SqlInstance $config.Instance2016 -Database $config.Database -Confirm:$false
+
+Restore-DbaDatabase -SqlInstance $config.Instance2016 -Path $config.BackupFile -DatabaseName $Config.Database -useDestinationDefaultDirectories
