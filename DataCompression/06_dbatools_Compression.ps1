c721eb980968d2489dddde068ecf0588b9217a01
Jess Pomfret <jpomfret7@gmail.com>
2018-05-31 13:58:17 -0700

    updates

--- DataCompression/06_dbatools_Compression.ps1
+++ DataCompression/06_dbatools_Compression.ps1
@@ -7,7 +7,9 @@ Get-Command -Module dbatools -Name *Compression*
 Get-Help Get-DbaDbCompression
 
 ## What should we compress - Tiger team SQL Script
-$results = Test-DbaDbCompression -SqlInstance localhost\sql2016 -Database AdventureWorks2016
+$results = Test-DbaDbCompression `
+-SqlInstance localhost\sql2016 `
+-Database AdventureWorks2016
 
 ## Let's look at the results for SalesOrderDetail
 $results |
@@ -15,4 +17,12 @@ Where-Object TableName -eq 'SalesOrderDetail' |
 Select-Object Schema, TableName, IndexName, IndexType, SizeCurrent, SizeRequested, CompressionTypeRecommendation
 
 ## Let's apply the suggested compression to the database
-Set-DbaDbCompression -SqlInstance localhost\sql2016 -Database AdventureWorks2016 -InputObject $results
+Set-DbaDbCompression `
+-SqlInstance localhost\sql2016 `
+-Database AdventureWorks2016 `
+-InputObject $results
+
+Set-DbaDbCompression `
+-SqlInstance localhost\sql2016 `
+-Database AdventureWorks2016 `
+-CompressionType Row ## Page, Recommended
