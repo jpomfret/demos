785bde44aa2ca02a973cca57c6a404c6d6665803
Jess Pomfret <jpomfret7@gmail.com>
2018-07-22 06:54:30 -0400

    comment out secondary db for now

--- DataCompression/01_Reset-Environment.ps1
+++ DataCompression/01_Reset-Environment.ps1
@@ -17,11 +17,16 @@ Write-Output ("Running with BackupFile: {0}" -f $config.BackupFile )
     Write-Output ("Running workload from {0}" -f $config.WorkloadFile)
     $null = Invoke-Sqlcmd2 -ServerInstance $config.Instance2016 -Database $config.PrimaryDatabase -InputFile $config.WorkloadFile -ParseGO
 
-## setup second db
-    Remove-DbaDatabase -SqlInstance $config.Instance2016 -Database $config.SecondaryDatabase -Confirm:$false
-    Restore-DbaDatabase -SqlInstance $config.Instance2016 -Path $config.BackupFile -DatabaseName $Config.SecondaryDatabase -useDestinationDefaultDirectories -DestinationFileSuffix '_2'
 
-
+    ## setup second db - invoke-sqlcmd2 not working with insert identity - manually setup
+    #Remove-DbaDatabase -SqlInstance $config.Instance2016 -Database $config.SecondaryDatabase -Confirm:$false
+#
+    #Write-Output ("Creating database {0}" -f $config.SecondaryDatabase)
+    #$null = Invoke-Sqlcmd2 -ServerInstance $config.Instance2016 -Database master -InputFile .\SetupQueries\Create_SalesOrderLarge_Database.sql -ParseGO
+    #Write-Output ("Setup database {0}" -f $config.SecondaryDatabase)
+    #$null = Invoke-Sqlcmd2 -ServerInstance $config.Instance2016 -Database $config.SecondaryDatabase -InputFile .\SetupQueries\Setup_SalesOrderLarge_Database.sql -ParseGO
+    #Write-Output ("Enlarge and compress {0}" -f $config.SecondaryDatabase)
+    #$null = Invoke-Sqlcmd2 -ServerInstance $config.Instance2016 -Database $config.SecondaryDatabase -InputFile .\SetupQueries\EnlargeAdventureWorks_Kehayias.sql -ParseGO
 
 Write-Output '-----'
 Write-Output 'Go filter SSMS for sales schema'
