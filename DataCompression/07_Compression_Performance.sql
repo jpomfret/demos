962237173f468627b4e8a159be8c2bd387a9a8dd
Jess Pomfret <jpomfret7@gmail.com>
2018-07-24 19:04:05 -0400

    rename

--- DataCompression/07_Compression_Performance.sql
+++ DataCompression/07_Compression_Performance.sql
@@ -3,14 +3,14 @@
 	-- Functions in the WHERE Clause - LEFT() - Forces a table scan, LIKE 'ASDF%' would be able to use an index
 	-- OPTION (MAXDOP 1) - Force only 1 CPU usage, keeps this test fair, probably best to presume the SQL engine knows best
 	-- DBCC FREEPROCCACHE -- Removes all elements from the plan cache
-	-- DBCC CLEANBUFFERS -- Removes all clean buffers from the buffer pool 
+	-- DBCC CLEANBUFFERS -- Removes all clean buffers from the buffer pool
 
 -- Turn on CPU and IO stats
 SET STATISTICS TIME ON
 SET STATISTICS IO ON
 
 DBCC FREEPROCCACHE
-DBCC DROPCLEANBUFFERS 
+DBCC DROPCLEANBUFFERS
 
 select soh.SalesOrderID, *
 from sales.SalesOrderHeaderEnlarged soh
@@ -21,7 +21,7 @@ OPTION (MAXDOP 1)
 
 
 DBCC FREEPROCCACHE
-DBCC DROPCLEANBUFFERS 
+DBCC DROPCLEANBUFFERS
 
 select soh.SalesOrderID, soh.orderdate, soh.Freight, sod.LineTotal
 from sales.SalesOrderHeaderEnlarged_ROW soh
@@ -32,7 +32,7 @@ OPTION (MAXDOP 1)
 
 
 DBCC FREEPROCCACHE
-DBCC DROPCLEANBUFFERS 
+DBCC DROPCLEANBUFFERS
 
 select soh.SalesOrderID, *
 from sales.SalesOrderHeaderEnlarged_PAGE soh
