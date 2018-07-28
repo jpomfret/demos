b76a4371edfa23309ac0cf6885ae7766696cd801
Jess Pomfret <jpomfret7@gmail.com>
2018-07-28 13:33:50 -0400

    updates

---
+++ DataCompression/01_Page_Internals_ObjectSizeAndCompression.sql
@@ -0,0 +1,26 @@
+Use CompressTest
+
+SELECT
+	schema_name(obj.SCHEMA_ID) as SchemaName,
+	obj.name as TableName,
+	ind.name as IndexName,
+	ind.type_desc as IndexType,
+	pas.row_count as NumberOfRows,
+	pas.used_page_count as UsedPageCount,
+	(pas.used_page_count * 8)/1024 as SizeUsedMB,
+	par.data_compression_desc as DataCompression,
+	(pas.reserved_page_count * 8)/1024 as SizeReservedMB
+FROM sys.objects obj
+INNER JOIN sys.indexes ind
+	ON obj.object_id = ind.object_id
+INNER JOIN sys.partitions par
+	ON par.index_id = ind.index_id
+	AND par.object_id = obj.object_id
+INNER JOIN sys.dm_db_partition_stats pas
+	ON pas.partition_id = par.partition_id
+WHERE obj.schema_id <> 4
+	--AND schema_name(obj.schema_id) = 'schemaName'
+	--AND obj.name = 'SalesOrderDetail'
+ORDER BY SizeUsedMB desc
+
+-- End of Demo 1
