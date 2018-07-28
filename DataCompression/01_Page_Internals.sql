349b5c68c7b991434839ae44fb2241d42a77096b
Jess Pomfret <jpomfret7@gmail.com>
2018-07-28 13:32:39 -0400

    added error so you can't run the whole script at once

--- DataCompression/01_Page_Internals.sql
+++ DataCompression/01_Page_Internals.sql
@@ -1,3 +1,6 @@
+RAISERROR(N'Did you mean to run the whole thing?',20,1) WITH LOG
+GO
+
 USE CompressTest
 
 DROP TABLE IF EXISTS employee
@@ -15,6 +18,7 @@ values	(1, 'Alex','Young','2 Sand Run','Akron'),
 		(2, 'Richard','Young','77 High St.','Akron'),
 		(3, 'Alexis','Young','1 First Ave.','Richfield')
 
+SELECT * FROM employee
 
 -- Find pages in Employee table
 DBCC IND ('CompressTest', 'employee', 1);
@@ -28,7 +32,7 @@ DBCC IND ('CompressTest', 'employee', 1);
 -- TF to output in messages instead of event log
 DBCC TRACEON (3604);
 GO
-DBCC PAGE('CompressTest',1,416,3)
+DBCC PAGE('CompressTest',1,456,3)
 -- pminlen - size of fixed length records		-- 512
 -- m_slotCnt - records on the page				-- 3
 -- m_freeCnt - bytes of free space on the page	-- 6545
@@ -39,7 +43,7 @@ WITH (DATA_COMPRESSION = ROW)
 -- Find pages in Employee table
 DBCC IND ('CompressTest', 'employee', 1);
 
-DBCC PAGE('CompressTest',1,424,3)
+DBCC PAGE('CompressTest',1,376,3)
 -- pminlen - size of fixed length records		-- 5
 -- m_slotCnt - records on the page				-- 3
 -- m_freeCnt - bytes of free space on the page	-- 7971
@@ -51,7 +55,7 @@ WITH (DATA_COMPRESSION = PAGE)
 -- Find pages in Employee table
 DBCC IND ('CompressTest', 'employee', 1);
 
-DBCC PAGE('CompressTest',1,464,3)
+DBCC PAGE('CompressTest',1,432,3)
 -- pminlen - size of fixed length records		-- 5
 -- m_slotCnt - records on the page				-- 3
 -- m_freeCnt - bytes of free space on the page	-- 7971
@@ -61,4 +65,12 @@ DBCC PAGE('CompressTest',1,464,3)
 -- 7971 free / 40 = 200 ish
 
 INSERT INTO employee (employeeId, firstName,lastName, address1, city)
-SELECT TOP 200 BusinessEntityID, FirstName, LastName, AddressLine1, CITY FROM AdventureWorks2014.HumanResources.vEmployee WHERE BusinessEntityID > 3
+SELECT TOP 200 BusinessEntityID, FirstName, LastName, AddressLine1, CITY FROM AdventureWorks2016.HumanResources.vEmployee WHERE BusinessEntityID > 3
+
+-- Find pages in Employee table
+DBCC IND ('CompressTest', 'employee', 1);
+
+DBCC PAGE('CompressTest',1,432,3)
+-- pminlen - size of fixed length records		-- 5
+-- m_slotCnt - records on the page				-- 3
+-- m_freeCnt - bytes of free space on the page	-- 7971
