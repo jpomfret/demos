88d0e8ab238642dc4a4208b9df8928e21b7d3695
Jess Pomfret <jpomfret@Domain.Merge>
2019-06-16 08:23:09 -0700

    renumber scripts

--- DSC_SQLServer/05_Configuration_Reporting.ps1
+++
@@ -1,9 +0,0 @@
-## Get the current configuration of the nodes, if a configuration exists
-Get-DscConfiguration -CimSession dscsvr2
-
-## Get configuration status for completed runs
-Get-DscConfigurationStatus -CimSession dscsvr2 | Select-Object *
-(Get-DscConfigurationStatus -CimSession dscsvr2).ResourcesInDesiredState
-
-## Test the current configuration
-Test-DscConfiguration -ComputerName DscSvr2 -Verbose
