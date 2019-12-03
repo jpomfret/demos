ec695b99a617cb585f97ecc2b53fbedad67f1a4c
jpomfret <jpomfret7@gmail.com>
2019-12-03 13:50:26 -0500

    add read in users from csv

--- LifeHacks_dbatools/Demos/03_Logins.ps1
+++ LifeHacks_dbatools/Demos/03_Logins.ps1
@@ -51,4 +51,21 @@ $pwdSplat = @{
 }
 Set-DbaLogin @pwdSplat
 
-## read in logins from csv?
+# Read in logins from csv
+## PS4 syntax!
+(Import-Csv .\users.csv).foreach{
+    $server = Connect-DbaInstance -SqlInstance $psitem.Server -SqlCredential $credential
+    New-DbaLogin -SqlInstance $server -Login $psitem.User -Password ($psitem.Password | ConvertTo-SecureString -asPlainText -Force)
+    New-DbaDbUser -SqlInstance $server -Login $psitem.User -Database $psitem.Database
+    Add-DbaDbRoleMember -SqlInstance $server -User $psitem.User -Database $psitem.Database -Role $psitem.Role.split(',') -Confirm:$false
+}
+
+<#
+## PS Version 3 & Lower
+foreach($user in $users) {
+    $server = Connect-DbaInstance -SqlInstance $user.Server -SqlCredential $credential
+    New-DbaLogin -SqlInstance $server -Login $user.User -Password ($user.Password | ConvertTo-SecureString -asPlainText -Force)
+    New-DbaDbUser -SqlInstance $server -Login $user.User -Database $user.Database
+    Add-DbaDbRoleMember -SqlInstance $server -User $user.User -Database $user.Database -Role $user.Role.split(',') -Confirm:$false
+}
+#>
